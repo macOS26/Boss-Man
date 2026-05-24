be4322841ad90d0d0ddb24cf3c2d703833f1c817
Agent! for MacOS <todd@starplayrx.com>
2026-05-23 20:24:32 -0400

    Editor DOM swatch matches game: purple+10% black tie

--- Boss-Man/Boss-Man/LevelEditorScene.swift
+++ Boss-Man/Boss-Man/LevelEditorScene.swift
@@ -449,7 +449,7 @@ class LevelEditorScene: SKScene {
         case Strings.Tile.boss2Char:
             addBoss(to: container, name: Strings.Boss.lumbergh,
                     body: NSColor.systemPink.withAlphaComponent(0.75),
-                    tie: .systemYellow, size: size)
+                    tie: NSColor.systemPurple.blended(withFraction: 0.10, of: .black) ?? .systemPurple, size: size)
         case Strings.Tile.boss3Char:
             addBoss(to: container, name: Strings.Boss.waddams,
                     body: .systemTeal,   tie: .systemBlue,   size: size)
