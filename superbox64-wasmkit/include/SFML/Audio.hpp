e1862f81feefdb8c34e2a7b6a9fdf473bae88b28
Agent! for MacOS <todd@starplayrx.com>
2026-06-09 00:04:50 -0400

    swift-wasm: remove C++ code and C++ build workflows

--- superbox64-wasmkit/include/SFML/Audio.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-// MARK: - Audio umbrella
-#include "System.hpp"
-#include "Audio/SoundSource.hpp"
-#include "Audio/SoundBuffer.hpp"
-#include "Audio/Sound.hpp"
-#include "Audio/Music.hpp"
--- superbox64-wasmkit/include/SFML/Window.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-// MARK: - Window umbrella
-#include "System.hpp"
-#include "Window/Keyboard.hpp"
-#include "Window/Mouse.hpp"
-#include "Window/Joystick.hpp"
-#include "Window/VideoMode.hpp"
-#include "Window/WindowStyle.hpp"
-#include "Window/Event.hpp"
